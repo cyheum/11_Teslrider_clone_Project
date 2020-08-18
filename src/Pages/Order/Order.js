1486e46669280159d3b2312954053afeef612bfc
최예흠 <cyheum@naver.com>
2020-08-18 18:57:09 +0900

    nav바 구성중 중간 commit

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Order.scss";
 
 class Order extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className="Order">
+        <nav className="headerNav">
+          <img alt="logo" src="./logo.png" />
+          <ol>
+            <li>자동차</li>
+            <li>외부</li>
+            <li>인테리어</li>
+            <li>오토파일럿</li>
+            <li>결제</li>
+          </ol>
+        </nav>
+      </section>
     );
   }
 }
